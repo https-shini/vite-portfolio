9900775347f602cac50de611bab07ce8d3445d66
Guilherme Souza <100307080+https-shini@users.noreply.github.com>
2024-05-04 22:56:17 -0300

    Add files via upload

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -1,20 +1,26 @@
 const darkMode = () => {
     const themeToggleBtns = document.querySelectorAll("#theme-toggle");
 
-    // State
-    const theme = localStorage.getItem("theme");
-
-    // On mount
-    theme && document.body.classList.add(theme);
+    // Verificar se o navegador suporta a preferência de tema
+    if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+        document.body.classList.add("dark-mode");
+        localStorage.setItem("theme", "dark-mode");
+    } else {
+        document.body.classList.add("light-mode");
+        localStorage.setItem("theme", "light-mode");
+    }
 
     // Handlers
     const handleThemeToggle = () => {
         document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("dark-mode");
         if (document.body.classList.contains("light-mode")) {
             localStorage.setItem("theme", "light-mode");
         } else {
-            localStorage.removeItem("theme");
-            document.body.removeAttribute("class");
+            localStorage.setItem("theme", "dark-mode");
         }
     };
 
